fad1219ff093c458dc795e77d8b36e40cdc6ba7b
champalal1 <31240587@vupune.ac.in>
2026-09-05 10:36:31 -0400

    Implement CarMake and CarModel Django models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,5 +1,7 @@
 # Uncomment the required imports before adding the code
 
+from .populate import initiate
+from .models import CarMake, CarModel
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
@@ -105,3 +107,21 @@ def registration(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+
+    if count == 0:
+        initiate()
+
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+
+    for car_model in car_models:
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
+
+    return JsonResponse({"CarModels": cars})
